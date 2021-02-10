2347e1bf0321c89484d5d180c3b852d7a1488fc2
shreshc <shresh.chaudhari@eronkan.com>
2021-02-10 18:37:55 +0530

    What’s needed to “Routing to real request handlers

---
+++ chap 10/requestHandlers.js
@@ -0,0 +1,10 @@
+function start() {
+    console.log("\nRequest handler 'start' was called.\n");
+}
+    
+function upload() {
+    console.log("\nRequest handler 'upload' was called.\n");
+}
+   
+exports.start = start;
+exports.upload = upload;
---
+++ chap 10/route.js
@@ -0,0 +1,10 @@
+function route(handle, pathname) {
+    console.log("About to route a request for " + pathname);
+    if (typeof handle[pathname] === 'functions') {
+        handle[pathname]();
+    } else {
+        console.log("No request handler found for " + pathname);
+    }
+}  
+
+exports.route = route;
