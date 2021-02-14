70a25aeb72e73fc9d5b016cb404003fbfed650ea
shreshc <shresh.chaudhari@eronkan.com>
2021-02-14 12:05:30 +0530

    Handling POST requests

---
+++ chap 13/router.js
@@ -0,0 +1,14 @@
+function route(handle, pathname, response, postData) {
+    console.log("About to route a request for " + pathname);
+    if (typeof handle[pathname] === 'function') {
+        handle[pathname](response, postData);
+    } 
+    else {
+        console.log("\nNo request handler found for " + pathname);
+
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Error: 404 Not found");
+        response.end();
+    }
+}
+exports.route = route;
